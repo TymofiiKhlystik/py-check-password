a47fbf41e72b4d743308b0ea1ee0a9103701c803
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-02 22:58:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+@pytest.mark.parametrize(
+    "password, expected_result",
+    [
+        ("Abc12345!", True),
+        ("abc123!", False),
+        ("Abc1234567890", False),
+        ("abc1234567890!", False),
+        ("Abc!123", False),
+        ("abc1234567890%", False),
+    ]
+)
+def test_check_password(password, expected_result):
+    assert check_password(password) == expected_result, f"Expected {expected_result} for password '{password}'"
