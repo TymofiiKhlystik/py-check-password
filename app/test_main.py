3296e75ae80b7df9ecbf7195bcc46e0cfd4e104c
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-02 23:08:34 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,9 +5,15 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password, expected_result",
     [
+        ("qwerty1", False),
+        ("Str@qwertytworty25123ng", False),
+        ("!qwerty@", False),
+        ("qwertyuio@", False),
+        ("QWERTYUIOPASDFGHJKLZXCVBNM12345", False),
+        ("Qwertyu123!@#sswwssdwswsd", False),
+        ("Qwerties#1ц", False),
+        ("qwerties#1", False),
         ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False),
     ]
 )
 def test_check_password(password: str, expected_result: bool) -> None:
